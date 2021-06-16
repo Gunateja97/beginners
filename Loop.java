e4fdfeac363b9f9c03c9ca2e2a532c652712be7f
Gunateja97 <85835565+Gunateja97@users.noreply.github.com>
2021-06-16 14:49:03 +0530

    Add files via upload

---
+++ Loop.java
@@ -0,0 +1,21 @@
+class Loop {
+    public static void main(String args[]) {
+        int age = 5;
+        int year = 2020;
+        while (age <= 7)
+       {
+           System.out.print("\tThe age is:" + age);
+            age++;
+        }
+        
+System.out.println();
+    
+          do
+            {
+            System.out.print("\tthe year is: " + year);
+             year++;
+            }
+        while (year <= 2023);
+
+    }
+}
